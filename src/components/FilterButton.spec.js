181a99d368cdfd970a97eb8d8b0f99aeece032dc
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2021-08-31 18:34:37 +0530

    Add FilterButton and it's test

---
+++ src/components/FilterButton.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import FilterButton from './FilterButton';
+
+describe('<FilterButton />', () => {
+    let filterButton, setFilter;
+
+    describe('clicking the send button', () => {
+        beforeEach(async () => {
+            setFilter = jest.fn().mockName('setFilter');
+            ({ filterButton } = render(<FilterButton name="All" setFilter={setFilter} isPressed={true} />));
+
+        });
+
+        it('Click filter button', () => {
+            document.querySelector("button").click();
+            expect(setFilter).toHaveBeenCalledWith("All");
+        });
+    });
+});
